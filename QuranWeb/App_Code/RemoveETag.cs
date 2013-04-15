735bcfa10e8e66a3fbbca8a28a0cf7eb73507ef4
Omar AL Zabir <omaralzabir@gmail.com>
2013-04-15 21:24:13 +0100

    Added missing files. Git is messed up

---
+++ QuranWeb/App_Code/RemoveETag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuranWeb.App_Code
+{
+    public class RemoveASPNETStuff : IHttpModule
+    {
+        public void Init(HttpApplication application)
+        {
+            application.PostReleaseRequestState += new EventHandler(application_PostReleaseRequestState);
+        }
+
+        void application_PostReleaseRequestState(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            HttpContext.Current.Response.Headers.Remove("ETag");
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
